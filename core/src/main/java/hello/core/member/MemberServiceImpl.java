e1ebdd8e5594358e7ff73a02aa7c028e494dbf1e
Nuri-Lee <nuri.lee.dev@gmail.com>
2021-09-07 23:46:00 +0900

    210907 |  Update Member Domain, Service, Repository

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    // 다형성에 의해 인터페이스인 MemberRepository 가 아닌 MemoryMemberRepository 가 호출된다.
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    /* MemberServiceImpl 의 문제점
+     *  MemberServiceImpl 은 MemberRepository 에 의존을 한다.
+     *  그런데 실제 할당하는 부분이 MemoryMemberRepository 이다 보니
+     *  MemoryMemberRepository 까지 의존하게 됐다. -> DIP 위반 */
+}
